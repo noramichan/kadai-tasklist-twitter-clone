f397ad5774a1158e8bb9d18a7cde9b7c291f0a43
yu.miwa <tory4ymiwa@gmail.com>
2020-04-17 08:57:47 +0000

    after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_155735) do
+ActiveRecord::Schema.define(version: 2020_04_17_082224) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
